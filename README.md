e247c4708895beea1bd04acbd4dff1050a0245e6
Rachel <racharon@gmail.com>
2021-05-08 11:57:30 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,87 @@
-# tictactoe
-# how to play
-etc
+# Animal Crossing Tic Tac Toe
+Inspired by Animal Crossing by nintendo
+
+# Welcome to the famous Tic Tac Toe Game
+https://rachellaron.github.io/tictactoe/
+
+# How to Play
+if your unfamiler with how to play Tic Tac Toe, use the boxes provided and click inside them to display your X or O symbol. 3 in a line will automically make you the winner!
+
+# Behind the Scenes (How it was built)
+This game was built using
+- HTML
+- CSS
+- JAVASCRIPT 
+with icons done by FontAwesome 
+
+# Extra Features
+You have the option to play music with playing the game, the music is from the orginal AninMal Crossing New Horizons Theme. You can also pause while playing. 
+When you have finish playing, click the reset button and it will reload the page for you.
+
+# Planning 
+- Beginning Steps
+
+    *My Understanding*
+
+    1. Display a webpage with a header & Div Container that contains the Tic Tac Toe Game
+    2. User clicks on one of the boxes and it displays an X, then user clicks on another Box and it displays an O 
+    3. If the gird has a line of either X or O's the user wins. 
+    - Row One, Two or Three
+    - Column One, Two or Three
+    - Diagonal Left & Right
+
+    4. Display a message saying either X(user One) or O(user two) has one 
+
+    5. Show button to play again
+
+    *Elements I need*
+
+    - `<header>`
+    - `<div class="">`
+    - `<div id="">`
+    - `<button>`
+  
+    *Coding Version*
+    ```jsx
+    1. Set Up Board
+        - Each Row have a class
+        - define which rows can be won with their classes
+    2. A way for users to click to add their X or their O
+    3. Check to see if the user has won
+        - Has the user got 3 in a row?
+            - Get all values in that Row
+            - Use querySelectorAll to find that class
+                - Repeat process till we can find values in a row, column or diagonal
+                - else Draw
+    4. Display restart button
+    ```
+
+    Another way 
+    ```jsx
+    1. Set up Board 
+    2. Get index of each board element([0],[1])
+    3. Define which index's are winning - var array = [0, 1, 2] , [3, 4, 5] 
+    4. User Clicks on specific box element (Index)
+    	- Check to see if the clicked boxes relate to board indexes
+    	- else 'draw'
+    5. Display restart button
+    ```
+# Extras that can be included
+1. User opens web page
+2. Shows mobile application
+3. Has Music Playing (music icon in top right to turn off or on
+4. Beginning Screen has 3 options - Single Player, Multiplayer & Play Local
+    - Single & Multi (Use Math.random & have computer to play against the user)
+5. Once player has entered game, showcase a scroll horizontal of 6 Icons to choose from 
+    - X, O, Emoji, Animal Crossing Character x3
+6. Once Character has been chose screen will show game same as the Logical version 
+7. Have 3 menu bar options at the bottom, Menu, Restart Game& Game Options
+    - Game Options, will have Icon Change, Time Limits, Board Same, Name Etc.
+
+WireFrame Planning was done on Figma. Simple Mobile App Design
+
+# Known Bugs 
+- When the user clicks the reload button it reloads the whole game instead of a certain frame
+- If you have won, you are able to click the other boxes to keep playing. 
+
+
--- main.js
+++ main.js
@@ -14,7 +14,6 @@ const boxSpace = [null, null, null, null, null, null, null, null, null];
 var playerOne = 'X';
 var playerTwo = 'O';
 var currentPlayer = playerOne;
-const emptyBoxes = [null, null, null, null, null, null, null, null, null];
 
 // Seeing which box is clicked from gameBoxes Array
 gameBoxes.forEach((gamebox, index) => {
@@ -24,73 +23,78 @@ gameBoxes.forEach((gamebox, index) => {
 // Using target to target which box was clicked and let the box know if current player is currently stored in that box to switch players and not allow that box to be changed
 function boxClicked(event) {
   const id = event.target.id;
-  console.log(id)
 
   // if the box space has an id of current player 
-
   if (!boxSpace[id]) {
     boxSpace[id] = currentPlayer;
     event.target.innerText = currentPlayer;
+    hasPlayerWon();
     currentPlayer = currentPlayer === playerTwo ? playerOne : playerTwo;
-    if (hasPlayerWon());
-
   }
+  if (ifFilled()) {
+    document.querySelector("h1").innerHTML = `DRAW!`;
+  }
+}
+
+// Function if its a draw
+function ifFilled() {
+  var ifFilled = (!boxSpace.includes(null));
+  return ifFilled;
 }
 
 //Check to see if player has won
 function hasPlayerWon() {
   // starting in top Left Corner
   if (boxSpace[0] === currentPlayer) {
-    // Up top win
+    // Up top win 
     if (boxSpace[1] === currentPlayer && boxSpace[2] === currentPlayer) {
-      console.log(`${currentPlayer} top straight across win`);
-
+      document.querySelector("h1").innerHTML = `${currentPlayer} WINS!`;
     }
     // left side win
-    else if (boxSpace[3] === currentPlayer && boxSpace[6] === currentPlayer) {
-      console.log(`${currentPlayer} top straight down win`);
+    if (boxSpace[3] === currentPlayer && boxSpace[6] === currentPlayer) {
+      document.querySelector("h1").innerHTML = `${currentPlayer} WINS!`;
     }
     // diagonal win
-    else if (boxSpace[4] === currentPlayer && boxSpace[8] === currentPlayer) {
-      console.log(`${currentPlayer} top diagonal win`);
+    if (boxSpace[4] === currentPlayer && boxSpace[8] === currentPlayer) {
+      document.querySelector("h1").innerHTML = `${currentPlayer} WINS!`;
     }
   }
 
   // Right Hand Corner Check
   // Right hand (index1)
-  else if (boxSpace[2] === currentPlayer) {
+  if (boxSpace[2] === currentPlayer) {
     // right side straight down win
     if (boxSpace[5] === currentPlayer && boxSpace[8] === currentPlayer) {
-      console.log(`${currentPlayer} right straight down win `);
+      document.querySelector("h1").innerHTML = `${currentPlayer} WINS!`;
     }
   }
 
   // Bottom Check
-  // Left Hand Corner (index6)
-  else if (boxSpace[6] === currentPlayer) {
-    // right side straight down win
+  // Left Hand Corner (index6)// THIS ONE IS BROKEN???
+  if (boxSpace[6] === currentPlayer) {
+    // diagonalwin
     if (boxSpace[4] === currentPlayer && boxSpace[2] === currentPlayer) {
-      console.log(`${currentPlayer} bottom diagonal win`);
+      document.querySelector("h1").innerHTML = `${currentPlayer} WINS!`;
     }
-    // diagonal win
-    else if (boxSpace[7] === currentPlayer && boxSpace[8] === currentPlayer) {
-      console.log(`${currentPlayer} bottom straight across win`);
+    // striaght win
+    if (boxSpace[7] === currentPlayer && boxSpace[8] === currentPlayer) {
+      document.querySelector("h1").innerHTML = `${currentPlayer} WINS!`;
     }
   }
 
   // Middle Check horizotnal
-  else if (boxSpace[3] === currentPlayer) {
+  if (boxSpace[3] === currentPlayer) {
     // right side straight down win
     if (boxSpace[4] === currentPlayer && boxSpace[5] === currentPlayer) {
-      console.log(`${currentPlayer} middle win horizontal`);
+      document.querySelector("h1").innerHTML = `${currentPlayer} WINS!`;
     }
   }
-  
+
   // Middle Check straight done
-  else if (boxSpace[1] === currentPlayer) {
+  if (boxSpace[1] === currentPlayer) {
     // right side straight down win
     if (boxSpace[4] === currentPlayer && boxSpace[7] === currentPlayer) {
-      console.log(`${currentPlayer} middle win straight down`);
+      document.querySelector("h1").innerHTML = `${currentPlayer} WINS!`;
     }
   }
 }
